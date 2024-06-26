7335b97e0d902bbbfed14fd17579c1f63ebff6dc
ochamu <171050612+ochamu@users.noreply.github.com>
2024-06-27 06:48:08 +0900

    セミコロン補充

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,9 +6,9 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await Promise.all(userList.map(test))
-  console.log(result)
-  return result
+  const result = await Promise.all(userList.map(test));
+  console.log(result);
+  return result;
 }
 
 function test(data) {
@@ -16,7 +16,7 @@ function test(data) {
     setTimeout(() => {
       data['full_name'] = data['family_name'] + ' ' + data['first_name'];
       resolve(data);
-      console.log(data)
+      console.log(data);
     }, 3000);
   })
 }
